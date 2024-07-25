b90ef5005f0cc3e795481c1f5d1f0f9656c82f8e
JeffreyMinucci <jminucci2@gmail.com>
2024-07-25 10:25:57 -0400

    improve docstrings

--- pybeepop/pybeepop.py
+++ pybeepop/pybeepop.py
@@ -1,132 +1,177 @@
-'''
+"""
 pybeepop - BeePop+ Wrapper for Python
 code by Jeff Minucci
 7/10/24
-'''
+"""
 
 import os
 import pandas as pd
 from .tools import BeePopModel
 import json
 
-class PyBeePop():
 
-    def __init__(self, lib_file, parameters = None, input_file = None, weather_file = 'Columbus',
-                 verbose = False):
-        '''
-        Initialize a pybeepop model object
+class PyBeePop:
+    """Wrapper for the BeePop+ honey bee colony simulation model"""
 
-        :param parameters: named dictionary of BeePop+ input parameters and their values
-        :param weather_file: Full path to the weather file e.g. C:/VarroaPop/weather.wea (must be .wea/.dvf/.wth) OR
-            one of either 'Columbus' (default), 'Sacramento', 'Phoenix', 'Yakima', 'Eau Claire', 'Jackson', or 'Durham'
-        :param verbose: True or false, print messages?
+    def __init__(
+        self, lib_file, parameter_file=None, weather_file=None, residue_file=None, verbose=False
+    ):
+        """Create a PyBeePop object connected to a BeePop+ shared library.
 
-        :return: Nothing
-        '''
-        #check file paths
+        Args:
+            lib_file (str): Path to the BeePop+ shared library.
+            parameters_file (str, optional): Path to a txt file of BeePop+ parameters where each line specifies
+                parameter=value. Defaults to None.
+            weather_file (str, optional): Path to a txt file containing weather data. For formatting info see
+                docs/weather_readme.txt. Defaults to None.
+            residue_file (str, optional): Path to a txt file containing pesticide residue data. Defaults to None.
+            verbose (bool, optional): Print additional debugging statements? Defaults to False.
+
+        Raises:
+            FileNotFoundError: If a provided file does not exist at the specified path.
+        """
+
+        # check file paths
         self.parent = os.path.dirname(os.path.abspath(__file__))
-        if lib_file is None:
-            lib_file = os.path.join(self.parent, 'files/exe/liblibvpop.so')
+        # if lib_file is None:
+        #    lib_file = os.path.join(self.parent, "files/exe/liblibvpop.so")
         if not os.path.isfile(lib_file):
-            raise FileNotFoundError('BeePop+ shared object library does not exist at path: {}!'.format(lib_file))
+            raise FileNotFoundError(
+                "BeePop+ shared object library does not exist at path: {}!".format(lib_file)
+            )
         self.lib_file = lib_file
-        #self.new_features = new_features # not being used?
+        if parameter_file is not None:
+            self.load_parameter_file(self.parameter_file)
+        else:
+            self.parameter_file = None
+        if weather_file is not None:
+            self.load_weather(weather_file)
+        else:
+            self.parameter_file = None
+        if residue_file is not None:
+            self.load_residue_file(self.residue_file)
+        else:
+            self.residue_file = None
+        # self.new_features = new_features # not being used?
         self.verbose = verbose
-        if parameters is not None:
-            if not isinstance(parameters, dict):
-                raise TypeError('parameters must be a named dictionary of BeePop+ parameters')
-        self.weather_file = None
-        self.contam_file = None
         self.output = None
-        self.input_file = None
         self.beepop = BeePopModel(self.lib_file, verbose=self.verbose)
-        self.parameters = self.beepop.set_parameters(parameters)
 
+    def set_parameters(self, parameters):
+        """Set BeePop+ parameters based on a dictionary {parameter: value}.
 
-    def set_parameters(self, parameters=None):
-        '''
-        Set or update the parameters 
+        Args:
+            parameters (dict): dictionary of parameteres {parameter: value}.
 
-        :param parameters: named dictionary of BeePop+ input parameters and their values
-
-        :return: Nothing
-        '''
+        Raises:
+            TypeError: If parameters is not a dict.
+        """
         if (parameters is not None) and (not isinstance(parameters, dict)):
-            raise TypeError('parameters must be a named dictionary of BeePop+ parameters')
+            raise TypeError("parameters must be a named dictionary of BeePop+ parameters")
         self.parameters = self.beepop.set_parameters(parameters)
-    
+
     def get_parameters(self):
+        """Return all parameters that have been set by the user."""
         return self.beepop.get_parameters()
 
-
     def load_weather(self, weather_file):
-        '''
-        Set the weather option
+        """Load a weather txt file. This should be a tab delimited txt file where each line denotes:
+        Date(MM/DD/YY), Max Temp (C), Min Temp (C), Avg Temp (C), Windspeed (m/s), Rainfall (mm),
+        Hours of daylight (optional).
 
-        :param weather_file: Full path to the weather file e.g. C:/VarroaPop/weather.wea (must be .wea/.dvf/.wth) OR
-            one of either 'Columbus' (default), 'Sacramento', 'Phoenix', 'Yakima', 'Eau Claire', 'Jackson', or 'Durham'
-        :return: Nothing
-        '''
+        Args:
+            weather_file (_type_): Path to the weather file .txt.
+
+        Raises:
+            FileNotFoundError: If the provided file does not exist at the specified path.
+        """
+        if not os.path.isfile(weather_file):
+            raise FileNotFoundError("Weather file does not exist at path: {}!".format(weather_file))
         self.weather_file = weather_file
         self.beepop.load_weather(self.weather_file)
-    
-    def load_input_file(self, input_file):
-        self.input_file = input_file
-        self.beepop.load_input_file(self.input_file)
-    
-    def load_contamination_file(self,file):
-        self.contam_file = file
-        self.beepop.load_contam_file(self.contam_file)
-    
+
+    def load_parameter_file(self, parameter_file):
+        """Load a .txt file of parameter values to set. Each line of the file is a string with the
+        format 'paramter=value'.
+
+        Args:
+            parameter_file (_type_): Path to a txt file of BeePop+ parameters.
+
+
+        Raises:
+            FileNotFoundError: If the provided file does not exist at the specified path.
+        """
+        if not os.path.isfile(parameter_file):
+            raise FileNotFoundError(
+                "Paramter file does not exist at path: {}!".format(parameter_file)
+            )
+        self.parameter_file = parameter_file
+        self.beepop.load_input_file(self.parameter_file)
+
+    def load_residue_file(self, residue_file):
+        """Load a .txt file of pesticide residues in pollen/nectar. File should be comma separated
+            with each line giving Date(MM/DD/YY), Concentration in nectar (g A.I. / g),
+            Concentration in pollen (g A.I. / g)
+
+        Args:
+            residue_file (_type_): Path to the residue .txt file.
+
+        Raises:
+            FileNotFoundError: If the provided file does not exist at the specified path.
+        """
+        if not os.path.isfile(residue_file):
+            raise FileNotFoundError("Residue file does not exist at path: {}!".format(residue_file))
+        self.residue_file = residue_file
+        self.beepop.load_contam_file(self.residue_file)
+
     def run_model(self):
-        '''
-        Run the BeePop+ model.
+        """_summary_
 
-        :return: output as a pandas DataFrame
-        '''
+        Raises:
+            RunetimeError: If the weather file has not yet been set.
 
-        #check to see if parameters have been supplied
+        Returns:
+            DataFrame: A DataFrame of the model results for the BeePop+ run.
+        """
+        # check to see if parameters have been supplied
         if (self.input_file is None) and (self.parameters is None):
-            pass
-            #raise Exception('Parameters must be set before running BeePop+!')
+            print("No parameters have been set. Running with defualt settings.")
         if self.weather_file is None:
-            raise Exception('Weather must be set before running BeePop+!')
+            raise RunetimeError("Weather must be set before running BeePop+!")
         self.output = self.beepop.run_beepop()
         return self.output
 
-    def get_output(self,format='dataframe'):
-        '''
-        Return the output from a BeePop+ run
-        
-        :param format: if 'dataframe', return pandas DataFrame object. If 'json', return json string.
-        :return: If format equals 'dataframe' (default), returns results as a pandas DataFrame. If format 
-        equals "json", returns results as a json string.
-        '''
-        if format == 'json':
-            result = json.dumps(self.output.to_dict(orient='list'))
+    def get_output(self, format="DataFrame"):
+        """Get the output from the last BeePop+ run.
+
+        Args:
+            format (str, optional): Return results as DataFrame ('DataFrame') or
+                JSON string ('json')? Defaults to "DataFrame".
+
+        Returns:
+            DataFrame or json str: A DataFrame or JSON string of the model results for the BeePop+ run.
+        """
+        if format == "json":
+            result = json.dumps(self.output.to_dict(orient="list"))
         else:
             result = self.output
         return result
-    
+
     def get_error_log(self):
+        """Return the BeePop+ session error log as a string for debugging."""
         return self.beepop.get_errors()
-    
+
     def get_info_log(self):
+        """Return the BeePop+ session info log as a string for debugging."""
         return self.beepop.get_info()
-    
+
     def version(self):
+        """Return the BeePop+ version as a string."""
         version = self.beepop.get_version()
-        #print(version)
         return version
-    
+
     def exit(self):
+        """Close the connection to the BeePop+ shared library."""
         self.beepop.close_library()
         del self.beepop
         return
-
-
-
-
-
-
-
--- pybeepop/tools.py
+++ pybeepop/tools.py
@@ -9,96 +9,144 @@ import io
 import ctypes
 import pandas as pd
 
-colnames = ["Date","Colony Size","Adult Drones","Adult Workers","Foragers", "Active Foragers", "Capped Drone Brood", "Capped Worker Brood",
-             "Drone Larvae", "Worker Larvae", "Drone Eggs", "Worker Eggs", "Total Eggs", "DD", "L", "N", "P", "dd", "l", "n", "Free Mites", "Drone Brood Mites",
-             "Worker Brood Mites", "Mites/Drone Cell", "Mites/Worker Cell", "Mites Dying", "Proportion Mites Dying",
-             "Colony Pollen (g)", "Pollen Pesticide Concentration", "Colony Nectar", "Nectar Pesticide Concentration",
-             "Dead Drone Larvae", "Dead Worker Larvae", "Dead Drone Adults", "Dead Worker Adults", "Dead Foragers",
-             "Queen Strength", "Average Temperature (celsius)", "Rain", "Min Temp", "Max Temp", "Daylight hours", "Forage Inc", "Forage Day"]
+colnames = [  # DataFrame column names for the BeePop+ output
+    "Date",
+    "Colony Size",
+    "Adult Drones",
+    "Adult Workers",
+    "Foragers",
+    "Active Foragers",
+    "Capped Drone Brood",
+    "Capped Worker Brood",
+    "Drone Larvae",
+    "Worker Larvae",
+    "Drone Eggs",
+    "Worker Eggs",
+    "Total Eggs",
+    "DD",
+    "L",
+    "N",
+    "P",
+    "dd",
+    "l",
+    "n",
+    "Free Mites",
+    "Drone Brood Mites",
+    "Worker Brood Mites",
+    "Mites/Drone Cell",
+    "Mites/Worker Cell",
+    "Mites Dying",
+    "Proportion Mites Dying",
+    "Colony Pollen (g)",
+    "Pollen Pesticide Concentration",
+    "Colony Nectar",
+    "Nectar Pesticide Concentration",
+    "Dead Drone Larvae",
+    "Dead Worker Larvae",
+    "Dead Drone Adults",
+    "Dead Worker Adults",
+    "Dead Foragers",
+    "Queen Strength",
+    "Average Temperature (celsius)",
+    "Rain",
+    "Min Temp",
+    "Max Temp",
+    "Daylight hours",
+    "Forage Inc",
+    "Forage Day",
+]
 
-# Utility function to convert a list of strings to bytes readble by the C++ library
-def  StringList2CPA(theList):
+
+def StringList2CPA(theList):
+    """Utility function to convert a list of strings to bytes readble by the C++ library"""
     theListBytes = []
     for i in range(len(theList)):
-        theListBytes.append(bytes(theList[i], 'utf-8'))
+        theListBytes.append(bytes(theList[i], "utf-8"))
     return theListBytes
 
+
 class BeePopModel:
-    """
-    BeePopModel
+    """Class of background functions to interface with the BeePop+ shared library using CTypes.
 
-    Class to interact with the BeePop+ linux library.
-
+    In most cases users would interact with a PyBeePop object instead of this class.
     """
 
-    def __init__(self, library_file, new_features=False, verbose = False):
+    def __init__(self, library_file, verbose=False):
+        """Initialize the connection to the BeePop+ shared library.
+
+        Args:
+            library_file (str): Path to the BeePop+ shared library.
+            verbose (bool, optional): Print debugging messages? Defaults to False.
+
+        Raises:
+            RuntimeError: If BeePop+ passes an error code on initialization.
+        """
         self.parameters = dict()
-        self.valid_parameters = pd.read_csv('docs/BeePop_exposed_parameters.csv', skiprows=1)['Exposed Variable Name'].tolist()
+        self.valid_parameters = pd.read_csv("docs/BeePop_exposed_parameters.csv", skiprows=1)[
+            "Exposed Variable Name"
+        ].tolist()
         self.weather_file = None
         self.contam_file = None
-        #self.new_features = new_features
         self.verbose = verbose
         self.results = None
         self.lib = ctypes.CDLL(library_file)
         self.parent_dir = os.path.dirname(os.path.abspath(__file__))
         self.lib_status = None
-        weather_dir = os.path.join(self.parent_dir,"files/weather")
+        weather_dir = os.path.join(self.parent_dir, "files/weather")
         if self.lib.InitializeModel():  # Initialize model
-            lib_status = self.lib.InitializeModel()
             if self.verbose:
-                #print(lib_status)
-                print('Model initialized')
+                print("Model initialized.")
         else:
-            raise RuntimeError('BeePop+ could not be initialized.')
+            raise RuntimeError("BeePop+ could not be initialized.")
         self.clear_buffers()
-    
-    # clear C++ buffers
-    def clear_buffers(self):  
+
+    def clear_buffers(self):
+        """Clear C++ buffers in BeePop+"""
         if not self.lib.ClearResultsBuffer():  # Clear Results and weather lists
-            raise RuntimeError('Error clearing results buffer.')
+            raise RuntimeError("Error clearing results buffer.")
         if not self.lib.ClearErrorList():
-            raise RuntimeError('Error clearing error list')
+            raise RuntimeError("Error clearing error list")
         if not self.lib.ClearInfoList():
-            raise RuntimeError('Error clearing info')
-     
-    #Load parameters from a file
-    def load_input_file(self, in_file):   
+            raise RuntimeError("Error clearing info")
+
+    def load_input_file(self, in_file):
+        """Load txt file of BeePop+ parameters."""
         self.input_file = in_file
         icf = open(self.input_file)
         inputs = icf.readlines()
         icf.close()
-        input_d = dict(x.replace(" ","").replace("\n","").split("=") for x in inputs)
-        self.parameter_list_update(input_d) # update parameter dictionary
+        input_d = dict(x.replace(" ", "").replace("\n", "").split("=") for x in inputs)
+        self.parameter_list_update(input_d)  # update parameter dictionary
         inputlist = []
         for k, v in self.parameters.items():
-            inputlist.append('{}={}'.format(k, v))
+            inputlist.append("{}={}".format(k, v))
         self.send_pars_to_beepop(inputlist)
         return self.parameters
-    
-    # update the dictionary of parameters
-    def parameter_list_update(self,parameters):
+
+    def parameter_list_update(self, parameters):
+        """Update the internal tracking of set parameters with a dict of
+        parameters: values."""
         to_add = dict((k.lower(), v) for k, v in parameters.items())
         self.parameters.update(to_add)
-    
-    # set parameters based on a python dictionary
+
     def set_parameters(self, parameters=None):
-        refresh = False
+        """Set BeePop+ parameters based on a dict of parameters: values"""
         if parameters is not None:
             self.parameter_list_update(parameters)
         else:
-            refresh = True # is this refresh variable needed?
             if len(self.parameters) < 1:
                 return
         inputlist = []
         for k, v in self.parameters.items():
-            inputlist.append('{}={}'.format(k, v))
-        self.send_pars_to_beepop(inputlist, refresh=refresh)
+            inputlist.append("{}={}".format(k, v))
+        self.send_pars_to_beepop(inputlist)
         return self.parameters
-    
-    # interact with the BeePop+ library to set parameters from a list
+
     def send_pars_to_beepop(self, parameter_list, refresh=False):
-        for par in parameter_list: # check for invalid parameters
-            par_name = par.split('=')[0].lower()
+        """Call the BeePop+ interface function to set parameters from a list of
+        parameter=value strings"""
+        for par in parameter_list:  # check for invalid parameters
+            par_name = par.split("=")[0].lower()
             if par_name not in [x.lower() for x in self.valid_parameters]:
                 print("Warning: {} is not a valid parameter and will be ignored.".format(par_name))
         CPA = (ctypes.c_char_p * len(parameter_list))()
@@ -106,144 +154,139 @@ class BeePopModel:
         CPA[:] = inputlist_bytes
         if self.lib.SetICVariablesCPA(CPA, len(parameter_list)):
             if self.verbose and not refresh:
-                print('Updated parameters')
+                print("Updated parameters")
         else:
             raise RuntimeError("Error setting parameters")
-        #del CPA  # shouldn't be needed 
-        #del inputlist_bytes
-    
-    # access the current list of parameters defined by the user
+
     def get_parameters(self):
+        """Return the current dict of user defined parameters"""
         return self.parameters
 
-    # load weather file .txt into BeePop+
-    def load_weather(self, weather_file='durham'):
-        self.weather_file = weather_file
-        weather_dir = os.path.join(self.parent_dir,"files/weather")
-        self.weather_locs = {'columbus': os.path.join(weather_dir,'18815_grid_39.875_lat.wea'),
-                             'sacramento': os.path.join(weather_dir,'17482_grid_38.375_lat.wea'),
-                             'phoenix': os.path.join(weather_dir, '12564_grid_33.375_lat.wea'),
-                             'yakima': os.path.join(weather_dir, '25038_grid_46.375_lat.wea'),
-                             'eau claire': os.path.join(weather_dir, '23503_grid_44.875_lat.wea'),
-                             'jackson': os.path.join(weather_dir, '11708_grid_32.375_lat.wea'),
-                             'durham': os.path.join(weather_dir, '15057_grid_35.875_lat.wea')}
-        if self.weather_file.lower() in self.weather_locs.keys():
-            self.weather_file = self.weather_locs[self.weather_file.lower()]
+    def load_weather(self, weather_file=None):
+        """Load a txt based weather file into BeePop+ using the library interface."""
         if self.weather_file is not None:
             try:
                 wf = open(self.weather_file)
                 weatherlines = wf.readlines()
                 wf.close()
+                self.weather_file = weather_file
             except:
                 raise OSError("Weather file is invalid.")
             CPA = (ctypes.c_char_p * len(weatherlines))()
-            weatherline_bytes = StringList2CPA(weatherlines) 
+            weatherline_bytes = StringList2CPA(weatherlines)
             CPA[:] = weatherline_bytes
             if self.lib.SetWeatherCPA(CPA, len(weatherlines)):
                 if self.verbose:
-                    print('Loaded Weather')
+                    print("Loaded Weather")
             else:
                 raise RuntimeError("Error Loading Weather")
-            #del CPA  # shouldn't be needed
-            #del weatherline_bytes
         else:
             raise TypeError("Cannot set weather file to None")
-    
+
     def load_contam_file(self, contam_file):
-        self.contam_file = contam_file
+        """Load txt file of pesticide residues in pollen/nectar using the library interface."""
         try:
             ct = open(contam_file)
             contamlines = ct.readlines()
             ct.close()
+            self.contam_file = contam_file
         except:
-            raise OSError("Contamination file is invalid.")
+            raise OSError("Residue file is invalid.")
         CPA = (ctypes.c_char_p * len(contamlines))()
         contamlines_bytes = StringList2CPA(contamlines)
         CPA[:] = contamlines_bytes
         if self.lib.SetContaminationTableCPA(CPA, len(contamlines)):
             if self.verbose:
-                print('Loaded contamination file')
+                print("Loaded residue file")
         else:
-            raise RuntimeError("Error loading contamination file")
-        #del CPA  # shouldn't be needed
-        #del contamlines_bytes
-        
+            raise RuntimeError("Error loading residue file")
+
     def set_latitude(self, latitude):
+        """Set the latitude for calculation of day length using the library interface."""
         c_double_lat = ctypes.c_double(latitude)
-        if(self.lib.SetLatitude(c_double_lat)):
+        if self.lib.SetLatitude(c_double_lat):
             if self.verbose:
-                print('Set Latitude to: {}'.format(latitude))
+                print("Set Latitude to: {}".format(latitude))
         else:
-                print('Error setting latitude')
-       
+            print("Error setting latitude")
 
     def run_beepop(self):
+        """Run the BeePop+ model once using the previously set parameters and weather.
+
+        Returns:
+            DataFrame: A pandas DataFrame of daily BeePop+ outputs.
+        """
         if self.lib.RunSimulation():
             self.lib_status = 1
-        else :
+        else:
             self.lib_status = 2
             if self.verbose:
-                print('Error in sumulation')
+                print("Error in sumulation")
         # fetch results
         theCount = ctypes.c_int(0)
         p_Results = ctypes.POINTER(ctypes.c_char_p)()
-        if self.lib.GetResultsCPA(ctypes.byref(p_Results),ctypes.byref(theCount)):
-            # store Reaults
+        if self.lib.GetResultsCPA(ctypes.byref(p_Results), ctypes.byref(theCount)):
+            # store results
             n_result_lines = int(theCount.value)
             self.lib.ClearResultsBuffer()
             out_lines = []
-            for j in range(0, n_result_lines-1): 
-                out_lines.append(p_Results[j].decode('utf-8', errors='replace'))
-            out_str = io.StringIO('\n'.join(out_lines))
-            out_df = pd.read_csv(out_str, delim_whitespace=True, skiprows=3, names = colnames, dtype={'Date': str})
+            for j in range(0, n_result_lines - 1):
+                out_lines.append(p_Results[j].decode("utf-8", errors="replace"))
+            out_str = io.StringIO("\n".join(out_lines))
+            out_df = pd.read_csv(
+                out_str, delim_whitespace=True, skiprows=3, names=colnames, dtype={"Date": str}
+            )
             self.results = out_df
         else:
-            print('Error running BeePop+ and fetching results')
+            print("Error running BeePop+ and fetching results.")
         self.clear_buffers()
-        #del theCount  # shouldn't be needed but trying to fix progressive slowdown issue
-        #del p_Results
         return self.results
-        
+
     def write_results(self, file_path):
+        """Write previously generated BeePop+ outputs to a csv file."""
         results_file = file_path
         if self.results is None:
             raise RuntimeError("There are no results to write. Please run the model first")
         self.results.to_csv(results_file, index=False)
         if self.verbose():
-            print('Wrote results to file')
-    
+            print("Wrote results to file")
+
     def get_errors(self):
+        """Return the BeePop+ error log as a string using the library interface."""
         p_Errors = ctypes.POINTER(ctypes.c_char_p)()
         count = ctypes.c_int()
         if self.lib.GetErrorListCPA(ctypes.byref(p_Errors), ctypes.byref(count)):
             error_lines = []
-            for j in range(count.value): 
-                error_lines.append(p_Errors[j].decode('utf-8', errors='replace'))
-            #self.lib.ClearErrorList()
+            for j in range(count.value):
+                error_lines.append(p_Errors[j].decode("utf-8", errors="replace"))
+            # self.lib.ClearErrorList()
         else:
             raise RuntimeError("Failed to get error log")
-        return '\n'.join(error_lines)
-    
+        return "\n".join(error_lines)
+
     def get_info(self):
+        """Return the BeePop+ info log as a string using the library interface."""
         p_Info = ctypes.POINTER(ctypes.c_char_p)()
         count = ctypes.c_int()
         if self.lib.GetInfoListCPA(ctypes.byref(p_Info), ctypes.byref(count)):
             info_lines = []
-            for j in range(count.value): 
-                info_lines.append(p_Info[j].decode('utf-8', errors='replace'))
-            #self.lib.ClearInfoList()
-        return '\n'.join(info_lines)
-    
+            for j in range(count.value):
+                info_lines.append(p_Info[j].decode("utf-8", errors="replace"))
+            # self.lib.ClearInfoList()
+        return "\n".join(info_lines)
+
     def get_version(self):
+        """Return the BeePop+ version as a string using the library interface."""
         buffsize = 16
         version_buffer = ctypes.create_string_buffer(buffsize)
         result = self.lib.GetLibVersionCP(version_buffer, buffsize)
         if result:
-            return version_buffer.value.decode('utf-8')
+            return version_buffer.value.decode("utf-8")
         else:
             raise RuntimeError("Failed to get library version")
-                 
+
     def close_library(self):
+        """Close connection to the library using CTypes."""
         dlclose_func = ctypes.CDLL(None).dlclose
         dlclose_func.argtypes = [ctypes.c_void_p]
         handle = self.lib._handle
